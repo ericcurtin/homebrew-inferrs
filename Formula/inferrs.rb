71df4279b3933823fd63e403fefff6072d972310
Eric Curtin <eric.curtin@docker.com>
2026-03-24 12:39:28 +0000

    Update formula to 9a418ad7dbb98eb6366cd69dd8891594e065ea3e

--- Formula/inferrs.rb
+++ Formula/inferrs.rb
@@ -1,26 +1,23 @@
 class Inferrs < Formula
   desc "A conservative-memory inference engine for LLMs"
   homepage "https://github.com/ericcurtin/inferrs"
-  version "0.0.0"
+  version "9a418ad7dbb98eb6366cd69dd8891594e065ea3e"
   license "Apache-2.0"
-
-  # This formula is updated automatically by CI on every merge to main.
-  # Install via: brew tap ericcurtin/inferrs && brew install inferrs
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "#{BASE}/inferrs-aarch64-apple-darwin.tar.gz"
+      sha256 "#{SHA_MACOS}"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "#{BASE}/inferrs-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "#{SHA_LINUX_X86}"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "#{BASE}/inferrs-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "#{SHA_LINUX_ARM}"
     end
   end
 
