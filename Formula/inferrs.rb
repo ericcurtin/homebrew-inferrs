c8fda29223efecd317e07fbe511e86d663a3c66e
Eric Curtin <eric.curtin@docker.com>
2026-03-24 12:16:47 +0000

    Initial formula placeholder

---
+++ Formula/inferrs.rb
@@ -0,0 +1,34 @@
+class Inferrs < Formula
+  desc "A conservative-memory inference engine for LLMs"
+  homepage "https://github.com/ericcurtin/inferrs"
+  version "0.0.0"
+  license "Apache-2.0"
+
+  # This formula is updated automatically by CI on every merge to main.
+  # Install via: brew tap ericcurtin/inferrs && brew install inferrs
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-aarch64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/ericcurtin/inferrs/releases/download/latest/inferrs-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "inferrs"
+  end
+
+  test do
+    assert_match "inferrs", shell_output("#{bin}/inferrs --help")
+  end
+end
